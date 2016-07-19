850dcde636095a870465a12f220f9b3c0bb45409
pliniogim <pliniogimenez@gmail.com>
2016-07-19 20:34:18 -0300

    Acerto arquivos que não foram incluídos no git

--- app/src/main/java/com/dlinkddns/pliniolgimenez/filmes/DetailActivity.java
+++ app/src/main/java/com/dlinkddns/pliniolgimenez/filmes/DetailActivity.java
@@ -1,12 +1,10 @@
 package com.dlinkddns.pliniolgimenez.filmes;
 
-import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
-import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
--- app/src/main/java/com/dlinkddns/pliniolgimenez/filmes/tmdbActivity.java
+++ app/src/main/java/com/dlinkddns/pliniolgimenez/filmes/tmdbActivity.java
@@ -10,7 +10,6 @@ import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -381,6 +380,7 @@ public class tmdbActivity extends AppCompatActivity {
                     gridview.setSelection(viewFocus);
                     gridview.setAdapter(new ImageAdapter(getActivity(), strings));
                     setAdapterListener();
+
                     //fileOutput();
                 }
             }
